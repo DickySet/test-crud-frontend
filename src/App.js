0ded8c348af906fc151b32393d9695d1c431f8cf
Dickysst <dicset234@gmail.com>
2023-01-26 08:56:47 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const[barang, setBarang] = useState();
+
+  const getBarang = async () => {
+    try {
+      let response = await axios.get('http://localhost:8800/')
+      // console.log(response.data)
+      setBarang(response.data)
+    } catch (e) {
+      console.log(e.message)
+    }
+  }
+
+  useEffect (() => {
+    getBarang();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="py-5">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Nama Barang</th>
+                  <th>Stok</th>
+                  <th>Jumlah Terjual</th>
+                  <th>jenis Barang</th>
+                  <th>tanggal Transaksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                      {barang.map((barang) => {
+                        return (
+                          <td key={barang.id}>
+                          {barang.nama_barang}
+                          {barang.stok}
+                          {barang.jumlah_terjual}
+                          {barang.jenis_barang}
+                          {barang.tanggal_transaksi}</td>
+                        )
+                      })}
+               </tr> 
+              </tbody>
+             </table>
+           </div>
+         </div>
+       </div>
+     </div>
   );
 }
 
